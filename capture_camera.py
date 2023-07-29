c23003af97c60dabf23adbe3f03e9dac15b93d11
Aan Prayogo <yulian@popbox.asia>
2023-07-29 12:14:47 +0700

    coba commit

--- capture_camera.py
+++ capture_camera.py
@@ -1,12 +1,33 @@
 import cv2
 from datetime import datetime
 import os
+import threading
+import time
 #droidcam = "192.168.0.123"   #RTSP
 
-cam = cv2.VideoCapture(1)  #default 0 untuk webcam laptop
+cam = cv2.VideoCapture(0)  #default 0 untuk webcam laptop
                             #selain 0 (1), (2) untuk webcam
 
 path = os.getcwd()
+
+
+def start_timetout2():
+    t1 = threading.Thread(target=timetout2)
+    t1.start()
+    
+def timetout2():
+    count = 5
+    print("timeout 2 mulai")
+
+    while True:
+        print(f"timeout !!")
+        #count-=1
+        time.sleep(1)
+        
+    #print("timeout 2 selesai")
+
+
+start_timetout2()
 
 while True:
     ret, frame = cam.read()
@@ -33,3 +54,4 @@ while True:
 
 cam.release()
 cv2.destroyAllWindows()
+
