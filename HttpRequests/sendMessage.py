13e800d1969bb83f682cf65d7af3d6767fa5bfe5
Aan Prayogo <yulian@popbox.asia>
2023-08-19 14:53:32 +0700

    tambah send message telegram

---
+++ HttpRequests/sendMessage.py
@@ -0,0 +1,10 @@
+import requests
+
+def send_msg(text):
+   token = "5936063047:AAEegGclhoRYVF2AnKsnWlAn-g0uwgLvT6o"
+   chat_id = "672670660"
+   url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text 
+   results = requests.get(url_req)
+   print(results.json())
+
+send_msg("Hello world!")
